fd2b11d20e09370dc270103324726a12f4e6f8af
BlackDev0222 <BlackDev0222@gmail.com>
2019-07-02 15:50:13 -0600

    update src\components\index.js

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import LayoutComponent from "./Layout";
+
+import DashboardComponent from "./Dashboard";
+import SignUpComponent from "./SignUp";
+
+export { LayoutComponent, DashboardComponent, SignUpComponent };
