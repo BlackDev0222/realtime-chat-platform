785c6b75aa7c1cc108d68cc8bbc79fe986b45e67
BlackDev0222 <BlackDev0222@gmail.com>
2020-02-17 14:50:17 -0700

    update src\components\Dashboard\index.jsx

---
+++ src/components/Dashboard/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const DashboardComponent = () => {
+  return (
+    <>
+      <div className="text-3xl text-center mt-5">
+        Welcome to Realtime Chat Platform!!!
+      </div>
+      <div className="text-2xl text-center mt-5">
+        Thanks for joining us.
+        <br />
+        Let's learn! Let's discuss!
+      </div>
+    </>
+  );
+};
+
+export default DashboardComponent;
